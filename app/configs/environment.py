162e14f7260c113616037ad2ad35394c89088b36
miko <islamimanhod@gmail.com>
2023-04-13 22:52:14 +0600

    feat: define Settings class to parse .env file, create fastapi app

---
+++ app/configs/environment.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings, RedisDsn
+
+
+class Settings(BaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: str
+    MINIO_CLIENT_USER: str
+    MINIO_CLIENT_PASSWORD: str
+
+    @property
+    def redis_uri(self) -> RedisDsn:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
